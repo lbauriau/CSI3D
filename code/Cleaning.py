3cc43f2bf7da3e8a78011bb0bf4be31a3143541f
Nathan Barraud <nathbarraud31@gmail.com>
2024-10-25 12:55:43 +0200

    Ajout de la fonction getOutputGates, changement de l'initialisation et ajout d'attributs dans la classe patch, changement de structure: de dico à liste et ref vers les instances

--- code/Cleaning.py
+++ code/Cleaning.py
@@ -2,5 +2,17 @@ import decimateur_utils
 
 def cleaningConquest(vertices, faces):
     
-    firstGate = decimateur_utils.get_first_gate(vertices, faces)
-    print(f"bonjour")
+    firstGate = decimateur_utils.getFirstGate(faces)
+    
+    fifo = [firstGate]
+    
+    #Tant qu'il y en a on traite les gates de la fifo
+    while len(fifo) > 0:
+        
+        current_gate = fifo[0]
+        current_patch = decimateur_utils.Patch(0,current_gate)
+        outputGates = current_patch.getOutputGates()
+        fifo += outputGates
+        
+        fifo.pop(0)
+        print(f"len(fifo) = {len(fifo)}")
