1cc49d2eeab3b377982fadfff71c0d71091a142e
Nathan Barraud <nathbarraud31@gmail.com>
2024-11-15 08:53:40 +0100

    Added Null Patch Attribute

--- code/decimateur_utils.py
+++ code/decimateur_utils.py
@@ -43,48 +43,52 @@ class Vertex:
 
 class Patch:
 
-    def __init__(self, id, entry_gate):
+    def __init__(self, id, entry_gate, isNullPatch):
 
         self.id = id
+        self.isNullPatch = isNullPatch;
         self.entry_gate = entry_gate
         self.center_vertex = entry_gate.getFrontVertex()
         self.boundingVertices = []
 
-        def findnextface(listfaces, vertex):
-            for f in listfaces:
-                print(f"verts:{[v.id for v in f.vertices]}")
-                if vertex in f.vertices and self.center_vertex in f.vertices:
-                    return f
-            return None
+        if isNullPatch:
+            self.boundingVertices = [ entry_gate.vertices[0], entry_gate.vertices[1], entry_gate.getFrontVertex() ]
+        else:
+            def findnextface(listfaces, vertex):
+                for f in listfaces:
+                    print(f"verts:{[v.id for v in f.vertices]}")
+                    if vertex in f.vertices and self.center_vertex in f.vertices:
+                        return f
+                return None
 
-        self.boundingVertices += self.entry_gate.vertices
+            self.boundingVertices += self.entry_gate.vertices
 
-        faceslist = self.center_vertex.attachedFaces[:]
-        faceslist.remove(self.entry_gate.frontFace)
-        current_vertex = self.entry_gate.vertices[1]
+            faceslist = self.center_vertex.attachedFaces[:]
+            faceslist.remove(self.entry_gate.frontFace)
+            current_vertex = self.entry_gate.vertices[1]
 
-        print("")
-        print( "Triage des bounding vertex...")
-        while faceslist:
-            print( "___________ Nouveau tour de boucle ______________")
             print("")
-            print(f"Current vertex id:{current_vertex.id}")
-            print(f"faceslist:{[f.id for f in faceslist]}")
-            face = findnextface(faceslist, current_vertex)
-            print(f"current_faces:{face.id}")
-            if face is not None:
-                for v in face.vertices:
-                    if v not in self.boundingVertices and v!= self.center_vertex:
-                        print (f"v.id :{v.id}")
-                        print (f"self.center_vertex.id :{self.center_vertex.id}")
-                        self.boundingVertices.append(v)
-
-                current_vertex = self.boundingVertices[-1]
-                print (f"new current_vertex.id :{current_vertex.id}")
+            print( "Triage des bounding vertex...")
+            while faceslist:
+                print( "___________ Nouveau tour de boucle ______________")
                 print("")
-                faceslist.remove(face)
-        print(f"bounding verts = {[v.id for v in self.boundingVertices]}")
-        print("")
+                print(f"Current vertex id:{current_vertex.id}")
+                print(f"faceslist:{[f.id for f in faceslist]}")
+                face = findnextface(faceslist, current_vertex)
+                print(f"current_faces:{face.id}")
+                if face is not None:
+                    for v in face.vertices:
+                        if v not in self.boundingVertices and v!= self.center_vertex:
+                            print (f"v.id :{v.id}")
+                            print (f"self.center_vertex.id :{self.center_vertex.id}")
+                            self.boundingVertices.append(v)
+
+                    current_vertex = self.boundingVertices[-1]
+                    print (f"new current_vertex.id :{current_vertex.id}")
+                    print("")
+                    faceslist.remove(face)
+            print(f"bounding verts = {[v.id for v in self.boundingVertices]}")
+            print("")
                     
     def getValence(self):
         #La valence d'un patch est en réalité la valence du vertex central, et
@@ -95,29 +99,49 @@ class Patch:
     def getOutputGates(self):
         output_gates = []
         
-        #On commence par le vertex de "droite" de l'entry gate
-        current_vertex = self.entry_gate.vertices[1]
-        
-        #-1 pour éviter l'entryGate
-        for i in range(len(self.boundingVertices) - 1):
-            
-            next_vertex_index = (self.boundingVertices.index(current_vertex) + 1)%len(self.boundingVertices)
+        if self.isNullPatch:
+            gate = self.entry_gate
+            first_outside_face = [f for f in gate.frontVertex.attachedFaces if
+                                (gate.vertices[0] in f.vertices and
+                                gate.vertices[1] not in f.vertices and
+                                gate.frontVertex in f.vertices)
+                                ]
+            if(len(first_outside_face) != 0):
+                first_outside_face = first_outside_face[0]
+                output_gates.append(Gate(first_outside_face, [ gate.vertices[0], gate.frontVertex ]))
 
-            next_vertex = self.boundingVertices[next_vertex_index]
+            second_outside_face = [f for f in gate.frontVertex.attachedFaces if
+                        (gate.vertices[1] in f.vertices and
+                        gate.vertices[0] not in f.vertices and
+                        gate.frontVertex in f.vertices)
+                        ]
+            if(len(second_outside_face) != 0):
+                second_outside_face = second_outside_face[0]
+                output_gates.append(Gate(second_outside_face, [ gate.vertices[1], gate.frontVertex ]))
+        else:
+            #On commence par le vertex de "droite" de l'entry gate
+            current_vertex = self.entry_gate.vertices[1]
+        
+            #-1 pour éviter l'entryGate
+            for i in range(len(self.boundingVertices) - 1):
             
-            #On récupère maintenant la face exterieure au patch liant current_vertex avec next_vertex.
-            #Elle dois contenir ces deux derniers vertex mais pas le center_vertex
-            next_outside_face = [f for f in current_vertex.attachedFaces if
-                            next_vertex in f.vertices and
-                            self.center_vertex not in f.vertices]
+                next_vertex_index = (self.boundingVertices.index(current_vertex) + 1)%len(self.boundingVertices)
+
+                next_vertex = self.boundingVertices[next_vertex_index]
             
-            #Si on ne trouve pas de face répondant aux contriantes on ne trouvera pas d'output gate pour le
-            #current vertex (c'est possible qu'il n'existe pas de faces en dehors du patch)
-            if(len(next_outside_face) != 0):
-                next_outside_face = next_outside_face[0]
-                #On ajoute la nouvelle gate aux output_gates
-                output_gates.append(Gate(next_outside_face, [current_vertex, next_vertex]))
-            current_vertex = next_vertex
+                #On récupère maintenant la face exterieure au patch liant current_vertex avec next_vertex.
+                #Elle dois contenir ces deux derniers vertex mais pas le center_vertex
+                next_outside_face = [f for f in current_vertex.attachedFaces if
+                                next_vertex in f.vertices and
+                                self.center_vertex not in f.vertices]
+            
+                #Si on ne trouve pas de face répondant aux contriantes on ne trouvera pas d'output gate pour le
+                #current vertex (c'est possible qu'il n'existe pas de faces en dehors du patch)
+                if(len(next_outside_face) != 0):
+                    next_outside_face = next_outside_face[0]
+                    #On ajoute la nouvelle gate aux output_gates
+                    output_gates.append(Gate(next_outside_face, [current_vertex, next_vertex]))
+                current_vertex = next_vertex
                 
         return output_gates
     
